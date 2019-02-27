93c0eea597d15c37f9744ac53d09573ebcc161da
Risto Saarelma <risto.saarelma@iki.fi>
2019-02-27 06:27:51 +0200

    Rust environment setup

---
+++ shell.nix
@@ -0,0 +1,34 @@
+with import <nixpkgs> {};
+
+stdenv.mkDerivation {
+  name = "wasm-env";
+  buildInputs = [
+    rustup
+    # Dev stuff cargo dependencies might need
+    cmake gcc zlib pkgconfig openssl
+
+    # Webassembly tools
+    wabt binaryen
+  ];
+  shellHook = ''
+    # Rust environment, use a local cargo dir
+    export CARGO_HOME=$PWD/.cargo
+    export PATH=$PATH:$CARGO_HOME/bin
+    export RUST_BACKTRACE=1
+
+    # Basic compiler
+    rustup install nightly
+    rustup default nightly
+    rustup update
+
+    # WASM setup
+    rustup target add wasm32-unknown-emscripten
+    NIX_ENFORCE_PURITY=0 cargo install wasm-pack
+
+    # Development helpers
+    rustup component add rls-preview rust-analysis rust-src
+    rustup component add rls-preview rust-analysis rust-src --toolchain nightly
+    rustup component add rustfmt-preview clippy-preview --toolchain nightly
+    NIX_ENFORCE_PURITY=0 cargo install cargo-outdated
+  '';
+}
