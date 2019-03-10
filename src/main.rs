798893a4af6323930ed649766d1708cba2b7f164
Risto Saarelma <risto.saarelma@iki.fi>
2019-03-10 09:30:48 +0200

    Option to echo debug outlines

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,10 @@ use structopt::{self, StructOpt};
 #[structopt(name = "otltool", about = "Outline processing tool")]
 enum Otltool {
     #[structopt(name = "echo", about = "Test by parsing and echoing stdin input")]
-    Echo,
+    Echo {
+        #[structopt(long = "debug", help = "Print debug versions of tokens")]
+        debug: bool,
+    },
 
     #[structopt(name = "tags", about = "Generate ctags file from local .otl files")]
     Tags,
@@ -21,19 +24,39 @@ enum Otltool {
     Anki,
 }
 
-fn echo() {
+fn echo(debug: bool) {
     let mut buf = String::new();
     io::stdin().read_to_string(&mut buf).unwrap();
 
     for tok in Lexer::new(&buf) {
-        print!("{}", tok);
+        use Token::*;
+        if debug {
+            match tok {
+                StartLine(n)
+                | BlockLine { depth: n, .. }
+                | StartPrefixBlock { depth: n, .. }
+                | StartPrefixBlock2 { depth: n, .. } => {
+                    for _ in 1..n {
+                        print!(" ");
+                    }
+                }
+                _ => {}
+            }
+            print!("{:?}", tok);
+            match tok {
+                NewLine | EndBlock(_) => println!(),
+                _ => {}
+            }
+        } else {
+            print!("{}", tok);
+        }
     }
 }
 
 fn main() {
     let opt = Otltool::from_args();
     match opt {
-        Otltool::Echo => echo(),
+        Otltool::Echo { debug } => echo(debug),
         _ => unimplemented!(),
     }
 }
