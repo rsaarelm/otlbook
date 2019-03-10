1ab7cf5691af2d29ebdfcbd6a85eb0a8036cf72b
Risto Saarelma <risto.saarelma@iki.fi>
2019-03-10 11:31:12 +0200

    Tool side work

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,23 @@
 use parser::{Lexer, Token};
+use std::collections::BTreeMap;
+use std::env;
+use std::fmt;
 use std::io::{self, Read};
+use std::path::{Path, PathBuf};
 use structopt::{self, StructOpt};
+use walkdir::{DirEntry, WalkDir};
+
+fn main() {
+    let opt = Otltool::from_args();
+    match opt {
+        Otltool::Echo { debug } => echo(debug),
+        Otltool::Tags => tags(),
+        _ => unimplemented!(),
+    }
+}
 
 #[derive(StructOpt)]
-#[structopt(name = "otltool", about = "Outline processing tool")]
+#[structopt(name = "otltool", about = "Outline file processing tool")]
 enum Otltool {
     #[structopt(name = "echo", about = "Test by parsing and echoing stdin input")]
     Echo {
@@ -15,13 +29,31 @@ enum Otltool {
     Tags,
 
     #[structopt(name = "jeval", about = "Pipe stdin outline through J evaluator")]
-    JEval,
+    JEval {
+        #[structopt(
+            long = "force",
+            help = "Ignore cached checksums, re-evaluate everything"
+        )]
+        force: bool,
+    },
 
     #[structopt(
         name = "anki",
         about = "Extract and upload Anki cards from local .otl files"
     )]
-    Anki,
+    Anki {
+        #[structopt(
+            long = "dump",
+            help = "Print tab-separated plaintext export instead of uploading to Anki"
+        )]
+        dump: bool,
+    },
+
+    #[structopt(
+        name = "server",
+        about = "Run a web server displaying outlines in HTML"
+    )]
+    Server,
 }
 
 fn echo(debug: bool) {
@@ -53,10 +85,94 @@ fn echo(debug: bool) {
     }
 }
 
-fn main() {
-    let opt = Otltool::from_args();
-    match opt {
-        Otltool::Echo { debug } => echo(debug),
-        _ => unimplemented!(),
+enum OutlineObject {
+    WikiTitle(String),
+    // Line(Vec<Token<String>>),
+    Block {
+        syntax: Option<String>,
+        is_preformatted: bool,
+        body: String,
+    },
+}
+
+struct Outline {
+    children: Vec<Outline>,
+}
+
+enum TagAddress {
+    LineNum(usize),
+    Search(String),
+}
+
+impl TagAddress {
+    fn start_of_file() -> TagAddress {
+        TagAddress::LineNum(0)
     }
+
+    fn otl_search(tagname: &str) -> TagAddress {
+        TagAddress::Search(format!(r"^\t\*{}$", tagname))
+    }
+}
+
+impl fmt::Display for TagAddress {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            TagAddress::LineNum(n) => write!(f, "{}", n),
+            TagAddress::Search(expr) => write!(f, "/{}/", expr),
+        }
+    }
+}
+
+struct CTags {
+    // Include depth in key so that tags deeper in the outline are give a lower priority in case
+    // there are multiple instances of the same tag name. Want the higher-up version to be more
+    // authoritative.
+    tags: BTreeMap<(String, usize), (String, TagAddress)>,
+}
+
+impl CTags {
+    fn insert_tag(&mut self, tag_name: &str, depth: usize, target_name: &str, path: &str) {
+        let key = (tag_name.to_string(), depth);
+        let addr = if depth == 0 {
+            TagAddress::start_of_file()
+        } else {
+            TagAddress::otl_search(target_name)
+        };
+
+        self.tags.insert(key, (path.to_string(), addr));
+    }
+}
+
+impl fmt::Display for CTags {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for ((tag, _), (path, addr)) in &self.tags {
+            writeln!(f, "{}\t{}\t{}", tag, path, addr)?;
+        }
+        Ok(())
+    }
+}
+
+fn tags() {
+    otl_paths(env::current_dir().expect("Invalid working directory"));
+}
+
+fn otl_paths(root: impl AsRef<Path>) -> impl Iterator<Item = PathBuf> {
+    fn is_otl(entry: &DirEntry) -> bool {
+        entry.file_type().is_file()
+            && entry
+                .file_name()
+                .to_str()
+                .map_or(false, |s| s.ends_with(".otl"))
+    }
+
+    WalkDir::new(root)
+        .into_iter()
+        .filter_map(|e| e.ok())
+        .filter_map(|e| {
+            if is_otl(&e) {
+                Some(e.into_path())
+            } else {
+                None
+            }
+        })
 }
