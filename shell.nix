aa2b031ce0a825064862cc95ab1e7ccb8d10d924
rsaarelm <risto.saarelma@iki.fi>
2020-05-13 15:03:25 +0300

    New Nix environment

--- shell.nix
+++ shell.nix
@@ -1,48 +1,34 @@
-with import <nixpkgs> {};
+let
+  pkgs = import <nixpkgs> {};
 
-stdenv.mkDerivation {
-  name = "wasm-env";
-  buildInputs = [
-    rustup
-    # Dev stuff cargo dependencies might need
+  log_level = "info";
+in
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    rustc cargo rustfmt rls cargo-outdated clippy
+
+    # Needed by cargo dependencies.
     cmake gcc zlib pkgconfig openssl
 
     # Webassembly tools
     wabt binaryen
 
-    # Server
-    lighttpd
-
-    # Useful stuff
-    html-tidy
-
     # Want this for the anki stuff
     anki
   ];
+
   shellHook = ''
-    # For cargo-outdated
+    # Dynamic linking for Vulkan stuff for wgpu graphics
     export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${
-      with pkgs.xlibs; lib.makeLibraryPath [ openssl zlib ]
+      with pkgs; pkgs.stdenv.lib.makeLibraryPath [ vulkan-loader ]
     }"
-    export PATH=$PATH:~/.cargo/bin
 
-    # Basic compiler
-    rustup install nightly
-    rustup default nightly
-    rustup update
+    # Run clippy without showing stuff I don't care about.
+    alias clippy="cargo clippy -- -A clippy::cast_lossless"
 
-    # WASM setup
-    rustup target add wasm32-unknown-emscripten
-    NIX_ENFORCE_PURITY=0 cargo install wasm-pack
-
-    # Development helpers
-    rustup component add rls-preview rust-analysis rust-src
-    rustup component add rls-preview rust-analysis rust-src --toolchain nightly
-    rustup component add rustfmt-preview clippy-preview --toolchain nightly
-    NIX_ENFORCE_PURITY=0 cargo install cargo-outdated
-
-    # Dev commands
-    alias run-webserver="lighttpd -D -f lighttpd.conf"
+    # FIXME: Current (2020-04-18) NixOS cargo-outdated is broken, you have to
+    # do this stupid thing. Remove alias when it's fixed.
+    alias cargo-outdated="cargo-outdated outdated"
 
     # Ensure AnkiConnect is installed
 
@@ -50,13 +36,18 @@ stdenv.mkDerivation {
     # could tell anki to install a plugin directly from the command line given
     # the plugin id...
 
-    if [ ! -d ~/.local/share/Anki2/addons21/2055492159 ]; then
-      echo "AnkiConnect plugin not found, installing..."
-      mkdir -p ~/.local/share/Anki2/addons21
-      pushd $(mktemp -d)
-      git clone https://github.com/FooSoft/anki-connect/
-      mv anki-connect/plugin ~/.local/share/Anki2/addons21/2055492159
-      popd
-    fi
+    # FIXME: This doesn't work with lorri shell...
+
+    # if [ ! -d ~/.local/share/Anki2/addons21/2055492159 ]; then
+    #   echo "AnkiConnect plugin not found, installing..."
+    #   mkdir -p ~/.local/share/Anki2/addons21
+    #   pushd $(mktemp -d)
+    #   git clone https://github.com/FooSoft/anki-connect/
+    #   mv anki-connect/plugin ~/.local/share/Anki2/addons21/2055492159
+    #   popd
+    # fi
   '';
+
+  RUST_BACKTRACE = "1";
+  RUST_LOG = "parser=${log_level},scraper=${log_level},olt=${log_level}";
 }
