ba0040e0e92483d7092f7ea951ac31a63cc749d7
rsaarelm <risto.saarelma@iki.fi>
2020-09-02 21:29:32 +0300

    Hello web server

--- src/otlwiki/hello.clj
+++ src/otlwiki/hello.clj
@@ -1,7 +1,18 @@
 (ns otlwiki.hello
-  (:gen-class))
+  (:gen-class)
+  (:require [org.httpkit.server :as server]))
+
+(defn handler
+  [req]
+  {:status  200
+   :headers {}
+   :body    "Hello server!"})
+
+(defn create-server
+  [port]
+  (server/run-server handler {:port port}))
 
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println "Starting server in http://localhost:8080/")
+  (create-server 8080))
