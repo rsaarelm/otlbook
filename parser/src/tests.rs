45ea5e1b2596ec45ec6c92f1feca3d0d866bbdcc
Risto Saarelma <risto.saarelma@iki.fi>
2021-04-09 18:27:30 +0300

    Testing

--- parser/src/tests.rs
+++ parser/src/tests.rs
@@ -2,6 +2,7 @@ use crate::{from_outline, outline, outline2::Outline2};
 use pretty_assertions::assert_eq;
 use serde::{de, Deserialize, Serialize};
 use std::fmt;
+use std::{collections::HashMap, iter::FromIterator};
 
 fn test<T: de::DeserializeOwned + Serialize + fmt::Debug + PartialEq>(
     outline: Outline2,
@@ -38,6 +39,7 @@ fn test_struct() {
         one_two: i32,
         num: i32,
         flag: bool,
+        tags: Vec<String>,
     }
 
     test(
@@ -47,16 +49,64 @@ fn test_struct() {
             one_two: 3,
             num: 123,
             flag: true,
+            ..Default::default()
         },
     );
 
+    // Defaults get filled in.
     test(
         outline!["title: xyzzy", "num: 123"],
         Struct {
             title: "xyzzy".to_string(),
-            one_two: 0,
             num: 123,
-            flag: false,
+            ..Default::default()
         },
+    );
+
+    // Tags is parsed differently because it's a sequence type.
+    test(
+        outline!["title: foo bar", "tags: foo bar"],
+        Struct {
+            title: "foo bar".to_string(),
+            tags: vec!["foo".to_string(), "bar".to_string()],
+            ..Default::default()
+        },
+    );
+
+    test(
+        outline!["title: xyzzy", "num: 123", "some: junk"],
+        Struct {
+            title: "xyzzy".to_string(),
+            num: 123,
+            ..Default::default()
+        },
+    );
+}
+
+#[test]
+fn test_seq() {
+    test(outline!["1", "2", "3", "4"], vec![1u32, 2, 3, 4]);
+
+    test(
+        outline!["foo", "bar", "baz"],
+        vec!["foo".to_string(), "bar".to_string(), "baz".to_string()],
+    );
+
+    test(
+        outline![["head line", "body1"], ["head2", "body2"]],
+        vec![
+            ("head line".to_string(), "body1".to_string()),
+            ("head2".to_string(), "body2".to_string()),
+        ],
+    );
+    test(
+        outline![["map line", "body1"], ["head2", "body2"]],
+        HashMap::<String, String>::from_iter(
+            vec![
+                ("map line".to_string(), "body1".to_string()),
+                ("head2".to_string(), "body2".to_string()),
+            ]
+            .into_iter(),
+        ),
     );
 }
