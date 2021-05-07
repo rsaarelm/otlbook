816aaf388f5453fbeb7e721a73fc7745d5f8bb1d
Risto Saarelma <risto.saarelma@iki.fi>
2021-05-07 17:31:07 +0300

    Parallelize outline loading

--- base/src/lib.rs
+++ base/src/lib.rs
@@ -6,9 +6,11 @@ pub use outline::{Outline, Section};
 pub type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
 pub fn load_collection() -> Result<Outline> {
+    use rayon::prelude::*;
     use walkdir::WalkDir;
+    use std::collections::BTreeMap;
 
-    log::info!("Loading collection");
+    log::info!("load_collection: Determining collection path");
     let root = if let Ok(path) = std::env::var("OTLBOOK_PATH") {
         PathBuf::from(path)
     } else if let Some(mut path) = dirs::home_dir() {
@@ -20,16 +22,28 @@ pub fn load_collection() -> Result<Outline> {
         )?;
     };
 
-    let mut result = Vec::new();
-    for entry in WalkDir::new(root).into_iter().filter_map(|e| e.ok()) {
-        if entry.path().extension() != Some(OsStr::new("otl")) {
-            continue;
-        }
-        if let Ok(outline) = Outline::try_from(entry.path()) {
-            result.push((Some(format!("{:?}", entry.path())), outline));
-        }
-    }
-    log::info!("Collection loaded");
+    log::info!("load_collection: Collecting .otl files");
 
-    Ok(Outline::from_iter(result))
+    let otl_extension = OsStr::new("otl");
+    let files: Vec<_> = WalkDir::new(root)
+        .into_iter()
+        .filter_map(|e| e.map(|e| e.path().to_path_buf()).ok())
+        .filter(|e| e.extension() == Some(otl_extension))
+        .collect();
+
+    log::info!("load_collection: Loading {} .otl files", files.len());
+    // Collect into BTreeMap so we automagically get the toplevel sorted
+    // lexically by filenames.
+    let sections: BTreeMap<_, _> = files
+        .into_par_iter()
+        .map(|p| {
+            (
+                Some(format!("{:?}", p)),
+                Outline::try_from(p.as_ref())
+                    .expect("load_collection: Failed to parse outline"),
+            )
+        })
+        .collect();
+    log::info!("load_collection: Merging loaded outlines");
+    Ok(Outline::from_iter(sections))
 }
