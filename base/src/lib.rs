d0402b3472b3a4b580bea121845532f568ec1cba
Risto Saarelma <risto.saarelma@iki.fi>
2021-06-28 14:17:50 +0300

    Add some datatypes to base crate

--- base/src/lib.rs
+++ base/src/lib.rs
@@ -1,7 +1,14 @@
 mod collection;
 pub use collection::Collection;
 
+mod date;
+pub use date::VagueDate;
+
 mod outline;
 pub use outline::{Outline, Section};
+
+mod symbol;
+pub use symbol::Sym;
+pub type Symbol = Sym<String>;
 
 pub type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
