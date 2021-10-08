c1dec22db162972eeef1972654fbc62a1b615147
Risto Saarelma <risto.saarelma@iki.fi>
2021-10-08 21:02:27 +0300

    Tree iterator

--- base/src/tree.rs
+++ base/src/tree.rs
@@ -1,4 +1,4 @@
-use std::{cell::Cell, fmt};
+use std::{cell::Cell, collections::VecDeque, fmt};
 
 pub struct Node<'a, T: 'a> {
     pub data: T,
@@ -6,6 +6,35 @@ pub struct Node<'a, T: 'a> {
     parent: Cell<Option<&'a Node<'a, T>>>,
     child: Cell<Option<&'a Node<'a, T>>>,
     sibling: Cell<Option<&'a Node<'a, T>>>,
+}
+
+pub struct DepthFirstNodes<'a, T: 'a> {
+    next: Option<&'a Node<'a, T>>,
+    pending: VecDeque<&'a Node<'a, T>>,
+}
+
+impl<'a, T> Iterator for DepthFirstNodes<'a, T> {
+    type Item = &'a Node<'a, T>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        // If out of siblings, start iterating the first node from the pending
+        // list.
+        if self.next.is_none() {
+            self.next = self.pending.pop_front();
+        }
+
+        // Push next node's first child to pending list, move next node cursor
+        // to its next sibling and yield the next node.
+        if let Some(node) = self.next {
+            self.next = node.sibling();
+            if let Some(child) = node.child() {
+                self.pending.push_back(child);
+            }
+            Some(node)
+        } else {
+            None
+        }
+    }
 }
 
 // TODO: Children iter (VecDeque for pending nodes)
@@ -108,6 +137,14 @@ impl<'a, T> Node<'a, T> {
             }
         }
     }
+
+    /// Iterate the tree nodes, breadth-first.
+    pub fn iter(&'a self) -> DepthFirstNodes<'a, T> {
+        DepthFirstNodes {
+            next: Some(self),
+            pending: Default::default(),
+        }
+    }
 }
 
 #[cfg(test)]
@@ -124,6 +161,16 @@ mod tests {
         root.append(a);
         root.append(new("B"));
 
+        a.append(new("X"));
+        a.append(new("Y"));
+
+        println!("Iter test");
+        for i in root.iter() {
+            print!("{} ", i.data);
+        }
+        println!();
+
+        println!("detach test");
         println!("{}", root);
         a.detach();
         println!("{}", root);
