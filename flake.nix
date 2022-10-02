e98510911a6799aab21998d41f1b5d505c1b6135
Risto Saarelma <risto.saarelma@iki.fi>
2022-10-02 12:56:46 +0300

    Touch up flake script

--- flake.nix
+++ flake.nix
@@ -2,47 +2,55 @@
   inputs = {
     naersk.url = "github:nmattia/naersk/master";
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
     utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, utils, naersk, ... }:
+  outputs = { self, nixpkgs, naersk, pre-commit-hooks, utils, ... }:
     utils.lib.eachDefaultSystem (system:
       let
+        pname = "otlbook";
         pkgs = import nixpkgs { inherit system; };
         naersk-lib = pkgs.callPackage naersk { };
-      in
-      {
-        defaultPackage = naersk-lib.buildPackage {
+      in rec {
+        checks = {
+          pre-commit-check = pre-commit-hooks.lib.${system}.run {
+            src = ./.;
+            hooks = { nixpkgs-fmt.enable = true; };
+          };
+        };
+
+        packages.default = naersk-lib.buildPackage {
           src = ./.;
-          pname = "otlbook";
           # Needed to build cargo dependencies.
           buildInputs = with pkgs; [ openssl pkg-config ];
         };
 
-        apps.default = utils.lib.mkApp {
-          drv = self.defaultPackage."${system}";
-        };
+        apps.default = utils.lib.mkApp { drv = packages.default; };
 
-        devShell = with pkgs; mkShell {
-          buildInputs = [
-            cargo
-            rustc
-            rustfmt
-            pre-commit
-            rust-analyzer
-            rustPackages.clippy
+        devShell = with pkgs;
+          mkShell {
+            buildInputs = [
+              cargo
+              rustc
+              rustfmt
+              pre-commit
+              rust-analyzer
+              rustPackages.clippy
 
-            # Needed by cargo dependencies.
-            openssl pkg-config
+              # Needed by cargo dependencies.
+              openssl
+              pkg-config
 
-            # Utils
-            tokei just
+              # Utils
+              tokei
+              just
 
-            # For anki SRS crate
-            # anki
-          ];
-          RUST_SRC_PATH = rustPlatform.rustLibSrc;
-          RUST_BACKTRACK = "1";
-        };
+              # For anki SRS crate
+              # anki
+            ];
+            RUST_SRC_PATH = rustPlatform.rustLibSrc;
+            RUST_BACKTRACK = "1";
+          };
       });
 }
